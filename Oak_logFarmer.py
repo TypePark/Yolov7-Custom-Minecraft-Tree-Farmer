1daf08775bbce59c5936616c24c7f2047c4c21d7
TypePark <111887669+TypePark@users.noreply.github.com>
2023-08-09 08:00:16 +0300

    Add files via upload

--- Oak_logFarmer.py
+++ Oak_logFarmer.py
@@ -9,19 +9,30 @@ import math
 import pyautogui
 
 # Load the YOLOv7 model from Torch Hub
-model = torch.hub.load('WongKinYiu/yolov7', 'custom', path_or_model='', force_reload=False)
+model = torch.hub.load('WongKinYiu/yolov7', 'custom',
+                       path_or_model='C:/Users/lolil/.cache/torch/hub/WongKinYiu_yolov7_main/runs/train/yolov7-custom32/weights/last.pt',
+                       force_reload=False)
 
 # Set up the screen capture
 sct = mss()
-monitor = {"top": 0, "left": 0, "width": 1920, "height": 1080}  # Adjust the monitor values to capture the desired region
+monitor = {"top": 0, "left": 0, "width": 1920,
+           "height": 1080}  # Adjust the monitor values to capture the desired region
 
-# Initialize variables for left-click holding and rotation
-holding_click = False
-click_start_time = 0
-rotation_angle = 0
-rotate_enabled = True
-last_detection_time = time.time()
-stop_looking = False  # Flag to indicate whether the AI should stop looking at the oak logs
+# Define the threshold distance
+threshold_distance = 1
+
+# Define the confidence score threshold
+confscore = 0.1
+
+boundbox = 100
+
+# Define the duration for mouse button press (in seconds)
+button_press_duration = 0.3
+
+# Initialize variables for spacebar pressing
+spacebar_pressed = False
+spacebar_start_time = 0
+spacebar_duration = 1.0  # 2 seconds
 
 while True:
     # Capture the screen frame
@@ -40,92 +51,75 @@ while True:
 
         if len(results.pred) > 0:
             # Find the closest bounding box
-            closest_distance = float('inf')  # Initialize with a very large value
-            closest_bbox_center_x = 0  # Initialize with a default value
-            closest_bbox_center_y = 0  # Initialize with a default value
+            closest_distance = float('inf')
+            closest_bbox_center_x = 0
+            closest_bbox_center_y = 0
+            closest_confidence = 0
 
             ai_x = monitor["width"] // 2
             ai_y = monitor["height"] // 2
 
             for detection in results.pred[0]:
-                # Calculate the center of the bounding box
-                bbox_center_x = int((detection[0] + detection[2]) / 2)
-                bbox_center_y = int((detection[1] + detection[3]) / 2)
+                if detection[4] > confscore:
+                    bbox_center_x = int((detection[0] + detection[2]) / 2)
+                    bbox_center_y = int((detection[1] + detection[3]) / 2)
+                    distance = math.sqrt((ai_x - bbox_center_x) ** 2 + (ai_y - bbox_center_y) ** 2)
 
-                # Calculate the distance between AI and the bounding box center
-                distance = math.sqrt((ai_x - bbox_center_x) ** 2 + (ai_y - bbox_center_y) ** 2)
+                    if (detection[2] - detection[0]) > boundbox and (detection[3] - detection[1]) > boundbox:
+                        if distance < threshold_distance:
+                            continue
 
-                if distance < closest_distance:
-                    closest_distance = distance
-                    closest_bbox_center_x = bbox_center_x
-                    closest_bbox_center_y = bbox_center_y
+                        if distance < closest_distance:
+                            closest_distance = distance
+                            closest_bbox_center_x = bbox_center_x
+                            closest_bbox_center_y = bbox_center_y
+                            closest_confidence = detection[4]
 
-            # Rotate the AI to face the closest bounding box if stop_looking is False
-            if not stop_looking:
+            if closest_confidence > 0:
                 dx = closest_bbox_center_x - ai_x
                 dy = closest_bbox_center_y - ai_y
                 angle = math.degrees(math.atan2(dy, dx))
-                pydirectinput.moveTo(ai_x, ai_y)  # Move the mouse to AI's position
-                pydirectinput.moveRel(dx, dy, duration=0.2)  # Move the mouse relative to AI's position to rotate
+                pydirectinput.moveTo(ai_x, ai_y)
+                pydirectinput.moveRel(dx, dy, duration=0.2)
 
-            # Check if the closest bounding box covers 25% of the screen
-            bbox_width = closest_bbox_center_x - results.pred[0][0][0]
-            bbox_height = closest_bbox_center_y - results.pred[0][0][1]
-            bbox_area = bbox_width * bbox_height
-            screen_area = monitor["width"] * monitor["height"]
-            bbox_coverage = bbox_area / screen_area
+                # Press the left mouse button for {button_press_duration} second
+                pyautogui.mouseDown(button='left')
+                time.sleep(button_press_duration)
+                pyautogui.mouseUp(button='left')
 
-            min_threshold = 0.08  # Minimum threshold value
-            max_threshold = 0.15  # Maximum threshold value
-            if min_threshold <= bbox_coverage <= max_threshold:
-                # Perform actions when the bounding box coverage is within the specified range
+                if (detection[2] - detection[0]) <= boundbox or (detection[3] - detection[1]) <= boundbox:
+                    print(f"Bounding box is smaller than {boundbox} x {boundbox} pixels. Pressing 'w'...")
 
-                # Stop looking at the oak logs
-                stop_looking = True
+                    # Press the 'w' key until the bounding box becomes larger or equal to boundbox
+                    while (detection[2] - detection[0]) <= boundbox or (detection[3] - detection[1]) <= boundbox:
+                        pydirectinput.keyDown('w')
+                        time.sleep(0.1)  # Adjust the delay as needed
+                        pydirectinput.keyUp('w')
+                        sct_img = sct.grab(monitor)
+                        frame = cv2.cvtColor(np.array(sct_img), cv2.COLOR_BGR2RGB)
+                        results = model(frame)
+                        detection = results.pred[0][0]  # Update the detection
 
-                # Reset rotation angle
-                rotation_angle = 0
+                    print("Bounding box size is now greater than or equal to", boundbox)
 
-                # Stop walking if the oak log covers 25% of the screen
-                pydirectinput.keyUp('w')
-                pydirectinput.keyUp('a')
-                pydirectinput.keyUp('d')
-                if not holding_click:
-                    # Start holding the left click for 3 seconds
-                    pydirectinput.mouseDown()
-                    holding_click = True
-                    click_start_time = time.time()
-                else:
-                    # Release the left click after 3 seconds and reset variables
-                    current_time = time.time()
-                    if current_time - click_start_time >= 2:
-                        pydirectinput.mouseUp()
-                        holding_click = False
-                        last_detection_time = time.time()
-                        rotate_enabled = True
+                    # Press the spacebar for 1 seconds if the bounding box dimensions are not increasing
+                    if not spacebar_pressed:
+                        spacebar_start_time = time.time()
+                        spacebar_pressed = True
+                    elif spacebar_pressed and (time.time() - spacebar_start_time) >= spacebar_duration:
+                        pydirectinput.keyDown('w')
+                        pydirectinput.keyDown('space')
+                        print("Ai stuck, pressing spacebar...")
+                        time.sleep(spacebar_duration)
+                        pydirectinput.keyUp('space')
+                        pydirectinput.keyUp('w')
+                        spacebar_pressed = False
+
             else:
-                # Continue walking and rotating if the oak log does not cover 25% of the screen
-                if not holding_click:
-                    pydirectinput.keyDown('w')
-                if holding_click:
-                    # Release the left click after 3 seconds and reset variables
-                    current_time = time.time()
-                    if current_time - click_start_time >= 2:
-                        pydirectinput.mouseUp()
-                        holding_click = False
-                        last_detection_time = time.time()
-                        stop_looking = False
+                print(f"There is no Oak_log present on the screen with a confidence score greater than {confscore}")
 
     except IndexError:
         print("IndexError: No detections found.")
-        pydirectinput.keyUp('w')
-        x_offset = 16  # Number of pixels to move along the X-axis
-        current_x, current_y = pyautogui.position()
-        new_x = current_x + x_offset
-        pyautogui.moveTo(new_x, current_y, duration=0.5)
-        time.sleep(1)
-
-
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
