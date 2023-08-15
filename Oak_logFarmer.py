badee5325dde85ef895a8c6142e699728e2e0ab9
TypePark <111887669+TypePark@users.noreply.github.com>
2023-08-15 13:05:00 +0300

    Update Oak_logFarmer.py

--- Oak_logFarmer.py
+++ Oak_logFarmer.py
@@ -8,32 +8,32 @@ import pydirectinput
 import math
 import pyautogui
 
-# Load the YOLOv7 model from Torch Hub
+# Loads the YOLOv7 model from Torch Hub
 model = torch.hub.load('WongKinYiu/yolov7', 'custom',  path_or_model='', force_reload=False)
 
-# Set up the screen capture
+# Sets up the screen capture
 sct = mss()
 monitor = {"top": 0, "left": 0, "width": 1920,
            "height": 1080}  # Adjust the monitor values to capture the desired region
 
-# Define the threshold distance
+# Defines the threshold distance
 threshold_distance = 1
 
-# Define the confidence score threshold
+# Defines the confidence score threshold
 confscore = 0.1
 
 boundbox = 100
 
-# Define the duration for mouse button press (in seconds)
+# Defines the duration for mouse button press (in seconds)
 button_press_duration = 0.3
 
-# Initialize variables for spacebar pressing
+# Initializes variables for spacebar pressing
 spacebar_pressed = False
 spacebar_start_time = 0
 spacebar_duration = 1.0  
 
 while True:
-    # Capture the screen frame
+    # Captures the screen frame
     sct_img = sct.grab(monitor)
     frame = cv2.cvtColor(np.array(sct_img), cv2.COLOR_BGR2RGB)
 
@@ -41,14 +41,14 @@ while True:
     results = model(frame)
 
     try:
-        # Render the detections on the frame
+        # Renders the detections on the frame
         rendered_frame = np.squeeze(results.render())
 
-        # Display the frame with detections
+        # Displays the frame with detections
         cv2.imshow('YOLO', rendered_frame)
 
         if len(results.pred) > 0:
-            # Find the closest bounding box
+            # Finds the closest bounding box
             closest_distance = float('inf')
             closest_bbox_center_x = 0
             closest_bbox_center_y = 0
@@ -80,7 +80,7 @@ while True:
                 pydirectinput.moveTo(ai_x, ai_y)
                 pydirectinput.moveRel(dx, dy, duration=0.2)
 
-                # Press the left mouse button for {button_press_duration} second
+                # Presses the left mouse button for {button_press_duration} second
                 pyautogui.mouseDown(button='left')
                 time.sleep(button_press_duration)
                 pyautogui.mouseUp(button='left')
@@ -88,7 +88,7 @@ while True:
                 if (detection[2] - detection[0]) <= boundbox or (detection[3] - detection[1]) <= boundbox:
                     print(f"Bounding box is smaller than {boundbox} x {boundbox} pixels. Pressing 'w'...")
 
-                    # Press the 'w' key until the bounding box becomes larger or equal to boundbox
+                    # Presses the 'w' key until the bounding box becomes larger or equal to boundbox
                     while (detection[2] - detection[0]) <= boundbox or (detection[3] - detection[1]) <= boundbox:
                         pydirectinput.keyDown('w')
                         time.sleep(0.1)  # Adjust the delay as needed
@@ -100,7 +100,7 @@ while True:
 
                     print("Bounding box size is now greater than or equal to", boundbox)
 
-                    # Press the spacebar for 1 seconds if the bounding box dimensions are not increasing
+                    # Presses the spacebar for 1 seconds if the bounding box dimensions are not increasing
                     if not spacebar_pressed:
                         spacebar_start_time = time.time()
                         spacebar_pressed = True
