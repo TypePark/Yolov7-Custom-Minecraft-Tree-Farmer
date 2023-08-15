3945e75a25d79c87bf54da55d1ab9d74c12d537a
TypePark <111887669+TypePark@users.noreply.github.com>
2023-08-15 13:25:27 +0300

    Update Oak_logScreenDetect.py

--- Oak_logScreenDetect.py
+++ Oak_logScreenDetect.py
@@ -4,11 +4,12 @@ import cv2
 import time
 from mss import mss
 from PIL import Image
+# This code is designed to test the detection quality of the model.
 
-# Load the YOLOv7 model from Torch Hub
+# Loads the YOLOv7 model from Torch Hub
 model = torch.hub.load('WongKinYiu/yolov7', 'custom',  path_or_model='', force_reload=False)
 
-# Set up the screen capture
+# Sets up the screen capture
 sct = mss()
 monitor = {"top": 0, "left": 0, "width": 1920, "height": 1080}  # Adjust the monitor values to capture the desired region
 
@@ -21,10 +22,10 @@ while True:
     # Make detections
     results = model(frame)
 
-    # Render the detections on the frame
+    # Renders the detections on the frame
     rendered_frame = np.squeeze(results.render())
 
-    # Display the frame with detections
+    # Displays the frame with detections
     cv2.imshow('YOLO', rendered_frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
